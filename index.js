4c9b53d46357b106c71baae586851d5c291c62bc
Sun Lee <sunlee.newyork@gmail.com>
2015-02-07 16:46:36 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+$(window).load(function() {
+
+	// jQuery Loading div that appears at the footer of page
+	$.mobile.loading().hide();
+
+	// Fade page into view
+	$("#landing").fadeIn(3000);		
+
+	$(".my-section").on('swipeleft', function () {
+		var section = '#';
+		section += $(this).data('next');
+		$.mobile.changePage(section);
+	});
+
+	$(".my-section-pages").on('swiperight', function() {
+		var page = '#';
+		page += $(this).data('prev');
+		$.mobile.changePage(page, { reverse: true } );
+	});
+
+});
