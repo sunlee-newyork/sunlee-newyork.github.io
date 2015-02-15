b2e42df090d8f35afb72ddab96588a763f7bcb8a
Sun Lee <sunlee.newyork@gmail.com>
2015-02-15 18:44:44 -0500

    finished search engine

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngSanitize']);
 
 app.directive('ngEnter', function () {
     return function (scope, element, attrs) {
@@ -16,26 +16,16 @@ app.directive('ngEnter', function () {
     };
 });
 
-// app.directive('results', function () {
-// 	return {
-// 		restrict: "A",
-// 		link: function (scope, elem, attrs) {
-// 			$(elem).show();
-// 		}
-// 	}
-// });
-
 app.controller('searchEngine', ['$scope', function ($scope) {
 	$scope.info = {
 		name: "Sun Lee",
 		education: "Stony Brook University - B.A. Linguistics",
 		email: "sunlee.newyork@gmail.com",
-		github: "github.com/sunlee-newyork",
-		linkedin: "linkedin.com/in/sunleenewyork/",
+		github: "www.github.com/sunlee-newyork",
+		linkedin: "www.linkedin.com/in/sunleenewyork",
 		location: "Brooklyn, NY",
 		number: "(917) 485-8278",
-		facebook: "facebook.com/sunlee1988",
-		instagram: "instagram.com/foodeatyum/",
+		instagram: "www.instagram.com/foodeatyum",
 		job: "Integration Engineer @Olapic",
 		position: "Integration Engineer @Olapic",
 		work: "Olapic",
@@ -43,16 +33,21 @@ app.controller('searchEngine', ['$scope', function ($scope) {
 		company: "Olapic",
 		resume: "Coming soon!",
 		portfolio: "Coming soon!",
-		projects: "Comming soon!"
+		projects: "Coming soon!",
+		age: "Nice try!",
+		contact: "sunlee.newyork@gmail.com"
 	}
+
 	$scope.showResult = function (input) {
+		var result = angular.element('#result');
+		result.css('height', '0');
+		result.css('opacity', '0');
 		angular.forEach($scope.info, function (value, index) {
 			if (input == index) {
 				$scope.searchResult = value;	
 			}
 		});
-		var result = angular.element('#result');
-		result.css('height', '150px');
+		result.css('min-height', '200px');
 		result.css('opacity', '1');
 	}
 }]);
